38128ed5c1384af7306c0ba664ec5c55fc14728b
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-08 17:53:58 +0530

    login page gui

---
+++ src/login_page.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class login_page {
+    private JPanel loginpage;
+    private JPanel toppanel;
+    private JPanel bottompanel;
+    private JTextField TextField1;
+    private JButton ADDButton;
+    private JButton RESETButton;
+    private JButton SUBButton;
+    private JTextField TextField2;
+    private JTextField TextField3;
+}
