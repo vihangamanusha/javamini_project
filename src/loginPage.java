0feda4c1f1963e81fba47bbe2d930c46ccc015e3
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-13 16:47:24 +0530

    login page guii chnge

--- src/loginPage.java
+++ src/loginPage.java
@@ -1,4 +1,11 @@
+import javax.swing.*;
+
 public class loginPage {
+    private JTextField username;
+    private JTextField password;
+    private JButton login;
+    private JButton RESET;
+
     public static void main(String[] args) {
 
     }
