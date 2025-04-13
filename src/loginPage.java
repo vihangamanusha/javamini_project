1f0ce905f65028ed8c79f8670fff1b8196948fb6
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-14 00:10:36 +0530

    login page guii chnge1

--- src/loginPage.java
+++ src/loginPage.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class loginPage {
     private JTextField username;
     private JTextField password;
     private JButton login;
     private JButton RESET;
+    private JPanel loginpage;
+
+    public loginPage() {
+        RESET.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                username.setText("");
+                password.setText("");
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("loginPage");
+        frame.setContentPane(new loginPage().loginpage);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
