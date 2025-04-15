6a617210e5115974e80ebdc8d79538d87b6431b3
PiumikaFdo <piumikafernando258@gmail.com>
2025-04-16 00:49:44 +0530

    Insert Attendance

--- src/Attendence_page.java
+++ src/Attendence_page.java
@@ -1,7 +1,26 @@
 import javax.swing.*;
 
-public class Attendence_page {
+public class Attendence_page extends JFrame{
     private JPanel panel1;
     private JTextField textField1;
-    private JCheckBox checkBox1;
+    private JTextField textField2;
+    private JTextField labelTextField;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+
+    public Attendence_page() {
+        setContentPane(panel1);
+        setTitle("Insert Attedance");
+        setSize(1000,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        Attendence_page insertAttendance=new Attendence_page();
+    }
+
+
+
 }
