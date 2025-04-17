3234ced6620003848dfccfe082274b6cb412a906
PiumikaFdo <piumikafernando258@gmail.com>
2025-04-17 13:36:00 +0530

    Gui of insert attendance form

--- src/Attendence_page.java
+++ src/Attendence_page.java
@@ -2,11 +2,17 @@ import javax.swing.*;
 
 public class Attendence_page extends JFrame{
     private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField labelTextField;
-    private JTextField textField4;
-    private JComboBox comboBox1;
+    private JFormattedTextField formattedTextField1;
+    private JComboBox ctype;
+    private JComboBox cid;
+    private JComboBox department;
+    private JComboBox comboBox5;
+    private JLabel a_type;
+    private JLabel date;
+    private JTextField sid;
+    private JComboBox semester;
+    private JButton InsertBtn;
+    private JButton resetBtn;
 
     public Attendence_page() {
         setContentPane(panel1);
@@ -16,10 +22,13 @@ public class Attendence_page extends JFrame{
         setVisible(true);
     }
 
+
+
     public static void main(String[] args) {
 
         Attendence_page insertAttendance=new Attendence_page();
     }
+
 
 
 
