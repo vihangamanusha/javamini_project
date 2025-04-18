b25318d509ee52e0b6faea537e1a0fb32d7d7f52
Chamod pinimal <chamod_20221397@fot.ruh.ac.lk>
2025-04-18 22:33:49 +0530

    create Lecturer Dashboard

---
+++ src/lecturer/LecturerDashboard.java
@@ -0,0 +1,60 @@
+package lecturer;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LecturerDashboard {
+    private JButton enterMarksButtonButton;
+    private JButton viewAttendanceButtonButton;
+    private JButton viewTimetablebuttonButton;
+    private JButton uploadCSVButtonButton;
+    private JButton exportDataButtonButton;
+    private JButton noticesButtonButton;
+    private JButton logoutButtonButton;
+
+    public LecturerDashboard() {
+        enterMarksButtonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewAttendanceButtonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewTimetablebuttonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        uploadCSVButtonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        exportDataButtonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        noticesButtonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logoutButtonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
